126b91cede3f4fd054c3c4576500f65fe2e98fda
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2023-07-03 15:33:03 -0300

    project structure and sum

--- ClienteCalculadora.java
+++ ClienteCalculadora.java
@@ -1,44 +1,34 @@
+import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.Scanner;
 
 public class ClienteCalculadora {
+
+  public static Scanner reader = new Scanner(System.in); // Leitor de entradas do usuario
+
   public static void main(String[] args) {
+
     try {
 
       String IP_SERVIDOR = "127.0.0.1"; // endereco do servidor na rede
       int PORTA_SERVIDOR = 1099; // porta padrao
 
       // Localiza o registry. É possível usar endereço/IP porta
-      Registry registry = LocateRegistry.getRegistry(IP_SERVIDOR,PORTA_SERVIDOR);
+      Registry registry = LocateRegistry.getRegistry(IP_SERVIDOR, PORTA_SERVIDOR);
       // Consulta o registry e obtém o stub para o objeto remoto
-      Calculadora calc = (Calculadora) registry
-          .lookup("calculadora");
+      Calculadora calc = (Calculadora) registry.lookup("calculadora");
+
       // A partir deste momento, chamadas à Caluladora podem ser
       // feitas como qualquer chamada a métodos
 
-      Scanner scan = new Scanner(System.in);
       int choice = -1;
+
       do {
-        System.out.println("MENU CALCULADORA");
-        System.out.println("=====================================");
-        System.out.println("1 - SOMA");
-        System.out.println("2 - SUBTRACAO");
-        System.out.println("3 - DIVISAO");
-        System.out.println("4 - MULTIPLICACAO");
-        System.out.println("5 - POTENCIA");
-        System.out.println("6 - RAIZ QUADRADA");
-        System.out.println("7 - PORCENTAGEM");
-        System.out.println("8 - LOGARITMO");
-        System.out.println("9 - CONVERSAO REAL -> DOLAR");
-        System.out.println("10 - CONVERSAO DOLAR -> REAL");
-        System.out.println("11 - CONVERSAO METROS -> KILOMETROS");
-        System.out.println("12 - CONVERSAO KILOMETROS -> METROS");
-        System.out.println("=====================================");
 
-        choice = scan.nextInt();
-
-        ClienteCalculadora.nextMenu(choice);
+        showMainMenu();
+        choice = reader.nextInt();
+        calculate(choice, calc);
 
       } while (choice != 0);
       /*
@@ -71,7 +61,99 @@ public class ClienteCalculadora {
     }
   }
 
-  private static void nextMenu(int choice) {
-    System.out.println(choice);
+  private static void showMainMenu() {
+    System.out.println("MENU CALCULADORA");
+    System.out.println("=====================================");
+    System.out.println("1 - SOMA");
+    System.out.println("2 - SUBTRACAO");
+    System.out.println("3 - DIVISAO");
+    System.out.println("4 - MULTIPLICACAO");
+    System.out.println("5 - POTENCIA");
+    System.out.println("6 - RAIZ QUADRADA");
+    System.out.println("7 - PORCENTAGEM");
+    System.out.println("8 - LOGARITMO");
+    System.out.println("9 - CONVERSAO REAL -> DOLAR");
+    System.out.println("10 - CONVERSAO DOLAR -> REAL");
+    System.out.println("11 - CONVERSAO METROS -> KILOMETROS");
+    System.out.println("12 - CONVERSAO KILOMETROS -> METROS");
+    System.out.println("=====================================");
   }
+
+  private static void calculate(int choice, Calculadora calc) {
+
+    try {
+      switch (choice) {
+        case 1:
+          System.out.println("SOMA");
+          System.out.println("================");
+
+          System.out.print("Digite o primeiro numero: ");
+          Numero num1 = new NumeroImpl(reader.nextInt());
+
+          System.out.print("Digite o segundo numero: ");
+          Numero num2 = new NumeroImpl(reader.nextInt());
+          System.out.println("================");
+
+          Numero result = calc.soma(num1, num2);
+          System.out.println("Resultado da soma: " + result.getValor());
+          waitForEnter();
+          break;
+
+        case 2:
+          System.out.println("SUBTRACAO");
+          break;
+
+        case 3:
+          System.out.println("DIVISAO");
+          break;
+
+        case 4:
+          System.out.println("MULTIPLICACAO");
+          break;
+
+        case 5:
+          System.out.println("POTENCIA");
+          break;
+
+        case 6:
+          System.out.println("RAIZ QUADRADA");
+          break;
+
+        case 7:
+          System.out.println("PORCENTAGEM");
+          break;
+
+        case 8:
+          System.out.println("LOGARITMO");
+          break;
+
+        case 9:
+          System.out.println("CONVERSAO REAL -> DOLAR");
+          break;
+
+        case 10:
+          System.out.println("CONVERSAO DOLAR -> REAL");
+          break;
+
+        case 11:
+          System.out.println("CONVERSAO METROS -> KILOMETROS");
+          break;
+
+        case 12:
+          System.out.println("CONVERSAO KILOMETROS -> METROS ");
+          break;
+
+      }
+
+    } catch (RemoteException e) {
+      System.out.println("Ocorreu um erro ao tentar se comunicar com o servidor. " + e.toString());
+    }
+  }
+
+  public static void waitForEnter() {
+    System.out.println("\nPressione enter para continuar...");
+    reader.nextLine();
+    reader.nextLine();
+  }
+
 }
