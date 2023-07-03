41d55a6b64d774955b436768cb667833214cde60
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2023-07-03 16:25:30 -0300

    calculator methods beta

--- Calculadora.java
+++ Calculadora.java
@@ -2,42 +2,42 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface Calculadora extends Remote {
-    public Numero somar(Numero a, Numero b)
-            throws RemoteException;
+        public Numero somar(Numero a, Numero b)
+                        throws RemoteException;
 
-    public Numero subtrair(Numero a, Numero b)
-            throws RemoteException;
+        public Numero subtrair(Numero a, Numero b)
+                        throws RemoteException;
 
-    public Numero multiplica(Numero a, Numero b)
-            throws RemoteException;
+        public Numero multiplicar(Numero a, Numero b)
+                        throws RemoteException;
 
-    public Numero dividir(Numero numerador, Numero denominador)
-            throws RemoteException, DivisaoPorZeroException;
+        public Numero dividir(Numero numerador, Numero denominador)
+                        throws RemoteException, DivisaoPorZeroException;
 
-    public Numero potencia(Numero base, Numero expoente)
-            throws RemoteException, DivisaoPorZeroException;
+        public Numero potencia(Numero base, Numero expoente)
+                        throws RemoteException;
 
-    public Numero raizQuadrada(Numero a)
-            throws RemoteException, DivisaoPorZeroException;
+        public Numero raizQuadrada(Numero a)
+                        throws RemoteException;
 
-    public Numero dolarParaReal(Numero dolares)
-            throws RemoteException, DivisaoPorZeroException;
+        public Numero dolarParaReal(Numero dolares)
+                        throws RemoteException;
 
-    public Numero realParaDolar(Numero reais)
-            throws RemoteException, DivisaoPorZeroException;
+        public Numero realParaDolar(Numero reais)
+                        throws RemoteException;
 
-    public Numero metroParaKilometro(Numero metros)
-            throws RemoteException, DivisaoPorZeroException;
+        public Numero metroParaKilometro(Numero metros)
+                        throws RemoteException;
 
-    public Numero kilometroParaMetro(Numero kilometros)
-            throws RemoteException, DivisaoPorZeroException;
+        public Numero kilometroParaMetro(Numero kilometros)
+                        throws RemoteException;
 
-    public Numero logaritmo(Numero metros)
-            throws RemoteException, DivisaoPorZeroException;
+        public Numero logaritmo(Numero logaritmando, Numero base)
+                        throws RemoteException;
 
-    public Numero porcentualParaDecimal(Numero a)
-            throws RemoteException, DivisaoPorZeroException;
+        public Numero porcentualParaDecimal(Numero a)
+                        throws RemoteException;
 
-    public Numero porcentagemDe(Numero a, Numero percentual)
-            throws RemoteException, DivisaoPorZeroException;
+        public Numero porcentagemDe(Numero a, Numero percentual)
+                        throws RemoteException;
 }
--- CalculadoraImpl.java
+++ CalculadoraImpl.java
@@ -2,94 +2,70 @@ import java.rmi.RemoteException;
 
 public class CalculadoraImpl implements Calculadora {
 
-    public Numero soma (Numero a, Numero b) {
-        return new NumeroImpl (a.getValor() + b.getValor());
+    public Numero somar(Numero a, Numero b) {
+        return new NumeroImpl(a.getValor() + b.getValor());
     };
 
-    public Numero subtrai (Numero a, Numero b) {
-        return new NumeroImpl (a.getValor() - b.getValor());
+    public Numero subtrair(Numero a, Numero b) {
+        return new NumeroImpl(a.getValor() - b.getValor());
     };
 
-    public Numero multiplica (Numero a, Numero b)  {
-        return new NumeroImpl (a.getValor() * b.getValor());
+    public Numero multiplicar(Numero a, Numero b) {
+        return new NumeroImpl(a.getValor() * b.getValor());
     };
 
-    public Numero divide (Numero a, Numero b)
-        throws DivisaoPorZeroException  {
-        if (b.getValor() == 0) throw new DivisaoPorZeroException();
-        return new NumeroImpl (a.getValor() / b.getValor());
+    public Numero dividir(Numero a, Numero b)
+            throws DivisaoPorZeroException {
+        if (b.getValor() == 0)
+            throw new DivisaoPorZeroException();
+        return new NumeroImpl(a.getValor() / b.getValor());
     }
 
     @Override
-    public Numero somar(Numero a, Numero b) throws RemoteException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'somar'");
+    public Numero potencia(Numero base, Numero expoente) throws RemoteException {
+        double result = Math.pow(base.getValor(), expoente.getValor());
+        return new NumeroImpl(result);
     }
 
     @Override
-    public Numero subtrair(Numero a, Numero b) throws RemoteException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'subtrair'");
+    public Numero raizQuadrada(Numero a) throws RemoteException {
+        double result = Math.sqrt(a.getValor());
+        return new NumeroImpl(result);
     }
 
     @Override
-    public Numero dividir(Numero numerador, Numero denominador) throws RemoteException, DivisaoPorZeroException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'dividir'");
+    public Numero dolarParaReal(Numero dolares) throws RemoteException {
+        return new NumeroImpl(dolares.getValor() * 4.8);
     }
 
     @Override
-    public Numero potencia(Numero base, Numero expoente) throws RemoteException, DivisaoPorZeroException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'potencia'");
+    public Numero realParaDolar(Numero reais) throws RemoteException {
+        return new NumeroImpl(reais.getValor() * 0.21);
     }
 
     @Override
-    public Numero raizQuadrada(Numero a) throws RemoteException, DivisaoPorZeroException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'raizQuadrada'");
+    public Numero metroParaKilometro(Numero metros) throws RemoteException {
+        return new NumeroImpl(metros.getValor() / 1000);
     }
 
     @Override
-    public Numero dolarParaReal(Numero dolares) throws RemoteException, DivisaoPorZeroException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'dolarParaReal'");
+    public Numero kilometroParaMetro(Numero kilometros) throws RemoteException {
+        return new NumeroImpl(kilometros.getValor() * 1000);
     }
 
     @Override
-    public Numero realParaDolar(Numero reais) throws RemoteException, DivisaoPorZeroException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'realParaDolar'");
+    public Numero logaritmo(Numero a, Numero b) throws RemoteException {
+        return new NumeroImpl(Math.log(a.getValor()) / Math.log(b.getValor()));
     }
 
     @Override
-    public Numero metroParaKilometro(Numero metros) throws RemoteException, DivisaoPorZeroException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'metroParaKilometro'");
+    public Numero porcentualParaDecimal(Numero percentual) throws RemoteException {
+        return new NumeroImpl(percentual.getValor() / 100);
     }
 
     @Override
-    public Numero kilometroParaMetro(Numero kilometros) throws RemoteException, DivisaoPorZeroException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'kilometroParaMetro'");
-    }
-
-    @Override
-    public Numero logaritmo(Numero metros) throws RemoteException, DivisaoPorZeroException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'logaritmo'");
-    }
-
-    @Override
-    public Numero porcentualParaDecimal(Numero a) throws RemoteException, DivisaoPorZeroException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'porcentualParaDecimal'");
-    }
-
-    @Override
-    public Numero porcentagemDe(Numero a, Numero percentual) throws RemoteException, DivisaoPorZeroException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'porcentagemDe'");
+    public Numero porcentagemDe(Numero a, Numero percentual) throws RemoteException {
+        return new NumeroImpl((percentual.getValor() * a.getValor()) / 100);
     };
 
 }
